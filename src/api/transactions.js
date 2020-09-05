220d284e0fe00ca13d21ae3cfee7bc0371baa3f4
clleashin <leajohn@naver.com>
2020-09-05 18:48:39 +0900

    edited transaction.js

--- src/api/transactions.js
+++ src/api/transactions.js
@@ -1,4 +1,5 @@
 import request from '../utils/request';
+import Axios from 'axios';
 
 export const fetchData = query => {
     return request({
@@ -7,3 +8,26 @@ export const fetchData = query => {
         params: query
     });
 };
+
+//export const addList = (data) => {
+//    return request({
+//      url: './table.json',
+//      method: 'post',
+//      data  
+//    });
+//};
+
+//export const editList = (data) => {
+//    return request({
+//        url: 'transactions/${data.transaction_id}',
+//        method: 'put',
+//       data
+//    });
+//};
+
+//export const deleteList = (transaction_id) => {
+//    return request({
+//        url: 'transactions/${transaction_id}',
+//        method: 'delete'
+//    });
+//};
